3ebcaa94353361060f04c30c8259e75dc15a2a73
skaug <hskaug@gmail.com>
2020-11-06 14:35:13 +0100

    Making TMB c++ part compiles

--- models/Netherlands/TMB_NLGreySeals.cpp
+++ models/Netherlands/TMB_NLGreySeals.cpp
@@ -19,13 +19,13 @@ Type objective_function<Type>::operator() ()
   //Input data
   DATA_VECTOR(pup);       // Observed pup counts NL
   DATA_VECTOR(daynr);     // Day number of NL pup count
-  DATA_VECTOR(yr_pup);    // Year of pup count
+  DATA_IVECTOR(yr_pup);    // Year of pup count
   DATA_VECTOR(pup_uk);    // Model-estimates pup-counts UK
   DATA_VECTOR(tot_uk);    // Model-estimates total NS-UK population size
   DATA_VECTOR(molt);      // Observed moult counts UK
-  DATA_VECTOR(yr_molt);   // Year of moult count
+  DATA_IVECTOR(yr_molt);   // Year of moult count
   DATA_VECTOR(smr);       // Observed summer counts
-  DATA_VECTOR(yr_smr);    // Year of summer count
+  DATA_IVECTOR(yr_smr);    // Year of summer count
   DATA_INTEGER(tmax);     // Number of years, equal to length pup counts UK (include in code below?)
   DATA_INTEGER(K_smr);    // Length of summer counts (include in code below?)
   DATA_INTEGER(K_molt);   // Length of molt counts (include in code below?)
@@ -54,7 +54,7 @@ Type objective_function<Type>::operator() ()
   Type surv_pup = phi_p * surv_ad; // is this working? 
  
   // Create empty matrixes and vectors
-  array<Type>   Nat(7,tmax);         // Numbers at age (7,tmax) or (tmax,7)
+  array<Type>   Nat(7,tmax);         // Numbers at age (7,tmax) or (tmax,7)  // To have multiple populations you need 3 dimensions.
   vector<Type>  N_est1(K_pup);       // 0 year olds in line with observed pup counts
   vector<Type>  bp(K_pup);           // birth arrival probability 
   vector<Type>  lp(K_pup);           // birth departure probability curve
@@ -63,47 +63,61 @@ Type objective_function<Type>::operator() ()
   vector<Type>  N17(K_smr);          // Total population size, same length as summer counts
   
   // Preliminary calculations for first year
-  Nat(2,0) = alpha_pup * pup_uk[1];
+  Nat(1,0) = alpha_pup * pup_uk(0);
+  Nat(2,0) = 0;
   Nat(3,0) = 0;
   Nat(4,0) = 0;
   Nat(5,0) = 0;
-  Nat(6,0) = 0;
-  Nat(7,0) = ad_ini;
-  Nat(1,0) = fec_ad * Nat(7,0) * frac_fem_sto;
+  Nat(6,0) = ad_ini;
+  Nat(0,0) = fec_ad * Nat(6,0) * frac_fem_sto;
     
   //---------------------------------
   // Calculate population trajectory
   for(int i=1;i<tmax;i++)
   {
-    Nat(0,i) = fec_ad * frac_fem_sto* Nat(7,i-1);  // I assume 0=age1
+    Nat(0,i) = fec_ad * frac_fem_sto* Nat(6,i-1);  // I assume 0=age1
     Nat(1,i) = alpha_pup * pup_uk(i) + surv_pup * Nat(0,i-1);
     
-    for(int j=2;j<5;j++) //j=2=age3
+    for(int j=2;j<6;j++) //j=2=age3
     {
-      Nat(j,i) = surv_ad * Nat(j,i-1);
+      Nat(j,i) = surv_ad * Nat(j-1,i-1);
     }
   
     Nat(6,i) = surv_ad * (Nat(5,i-1) + Nat(6,i-1));
    }
     
   // Calculate pup probabilities
-     for(int i=1;i<K_pup;i++)
+     for(int i=0;i<K_pup;i++){
              bp(i)= ilogit(-mean_day*beta1+beta1*daynr(i) -beta1*beta2*yr_pup(i));
              lp(i)= ilogit(-mean_day*beta1+beta1*daynr(i) -beta1*beta2*yr_pup(i)-beta1*pup_dur);
              pup_prop(i) = bp(i)-lp(i);
            }
    
   // Calculate expected pup counts
-      for(int i=1;i<K_pup;i++)
-        N_est1(i) <- 0.001+ Nat(0,yr_pup(i))*pup_prop(i);
+      for(int i=0;i<K_pup;i++)
+        N_est1(i) = 0.001 + Nat(0,yr_pup(i))*pup_prop(i);
              
+  
   // Calculate expected molt counts
-      for(int i=1;i<K_molt;i++)
-        N27(i) <- corf_sto * surv_pup * Nat.cut(1,yr_molt(ik)) + pow(surv_ad,106/365) * sum(Nat(2:7,yr_molt(i))) + alpha_molt * tot.uk(yr_molt(i)+1);
+        
+        for(int i=0;i<K_molt;i++){
+          Type tmp= Type(0.0);
+          for(int ii=1;ii<=6;ii++){
+            tmp = tmp + Nat(ii,yr_molt(i));
+          }
+        N27(i) = corf_sto * surv_pup * Nat(1,yr_molt(i)) + pow(surv_ad,106.0/365.0) * tmp + alpha_molt * tot_uk(yr_molt(i)+1);
+      }
           
   // Calculate expected molt counts
-      for(int i=1;i<K_smr;i++)
-        N17(i) <- corf_sto * surv_pup * Nat.cut(1,yr_smr(i)) + pow(surv_ad,207/365) * sum(Nat(2:7,yr_smr(i))) + alpha_smr * tot_uk(yr_molt(i)+1);
+      for(int i=0;i<K_smr;i++){
+        Type tmp= Type(0.0);
+        for(int ii=0;ii<=6;ii++){
+          tmp = tmp + Nat(ii,yr_smr(i));
+        }
+        
+          N17(i) = corf_sto * surv_pup * Nat(1,yr_smr(i)) + pow(surv_ad,207.0/365.0) * tmp + alpha_smr * tot_uk(yr_molt(i)+1);
+
+      }
   
   
    
@@ -116,29 +130,29 @@ Type objective_function<Type>::operator() ()
   // Likelihood contribution from pup counts
      for(int i=0;i< K_pup;i++)
      {
-     nll += -dpoisson(N_est1(i),pup(i),true);
+     nll += -dpois(pup(i),N_est1(i),true);
      }
 
   // Likelihood contribution from molt counts
      for(int i=0;i< K_molt;i++)
      {
-     nll += -dpoisson(N27(i),molt(i),true);
+     nll += -dpois(molt(i),N27(i),true);
      }
 
   // Likelihood contribution from summer counts
      for(int i=0;i< K_smr;i++)
      {
-     nll += -dpoisson(N17(i),smr(i),true);
+     nll += -dpois(smr(i),N17(i),true);
      }
 
   // Likelihood contribution from informative prior distibutions - EQ 11
-     nll += -dbeta(phi_a,1.6,1.2,true);
-     nll += -dbeta(phi_p,2.87,1.78,true);
-     nll += -dbeta(fec_a,2,1.5,true);
+     nll += -dbeta(phi_a,Type(1.6),Type(1.2),true);
+     nll += -dbeta(phi_p,Type(2.87),Type(1.78),true);
+     nll += -dbeta(fec_a,Type(2),Type(1.5),true);
      
   // The following priors are fixed; i.e. parameters should not be estimated by the model
-     nll += -dnorm(corf2,-1.0935,21.51307,true);
-     nll += -dbeta(beta_fem,2,8,true);
+     nll += -dnorm(corf2,Type(-1.0935),Type(21.51307),true);
+     nll += -dbeta(beta_fem,Type(2),Type(8),true);
          
   //----------------------------------------------------------------------------
   // Report section, define which quantities should have uncertainty estimates
